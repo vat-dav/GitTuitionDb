928b12ed54ac03d89d48d60fd8c7de7002fe5c48
Vatsal Dave <ac121655@avcol.school.nz>
2024-04-02 01:40:59 +1300

    I have added and tested all CRUD operations for all of my models and they are working perfectly. I am embarrased it took me this long to realise such a mistake, but am glad I found it.

---
+++ TuitionDb/Controllers/BatchFeesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TuitionDb.Areas.Identity.Data;
+using TuitionDbv1.Models;
+
+namespace TuitionDb.Controllers
+{
+    public class BatchFeesController : Controller
+    {
+        private readonly TuitionDbContext _context;
+
+        public BatchFeesController(TuitionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BatchFees
+        public async Task<IActionResult> Index()
+        {
+            var tuitionDbContext = _context.BatchFee.Include(b => b.Students);
+            return View(await tuitionDbContext.ToListAsync());
+        }
+
+        // GET: BatchFees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batchFee = await _context.BatchFee
+                .Include(b => b.Students)
+                .FirstOrDefaultAsync(m => m.FeeId == id);
+            if (batchFee == null)
+            {
+                return NotFound();
+            }
+
+            return View(batchFee);
+        }
+
+        // GET: BatchFees/Create
+        public IActionResult Create()
+        {
+            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "BillingAddress");
+            return View();
+        }
+
+        // POST: BatchFees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("FeeId,StudentId,AmountPaid,Received")] BatchFee batchFee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(batchFee);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "BillingAddress", batchFee.StudentId);
+            return View(batchFee);
+        }
+
+        // GET: BatchFees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batchFee = await _context.BatchFee.FindAsync(id);
+            if (batchFee == null)
+            {
+                return NotFound();
+            }
+            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "BillingAddress", batchFee.StudentId);
+            return View(batchFee);
+        }
+
+        // POST: BatchFees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("FeeId,StudentId,AmountPaid,Received")] BatchFee batchFee)
+        {
+            if (id != batchFee.FeeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(batchFee);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BatchFeeExists(batchFee.FeeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "BillingAddress", batchFee.StudentId);
+            return View(batchFee);
+        }
+
+        // GET: BatchFees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batchFee = await _context.BatchFee
+                .Include(b => b.Students)
+                .FirstOrDefaultAsync(m => m.FeeId == id);
+            if (batchFee == null)
+            {
+                return NotFound();
+            }
+
+            return View(batchFee);
+        }
+
+        // POST: BatchFees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var batchFee = await _context.BatchFee.FindAsync(id);
+            if (batchFee != null)
+            {
+                _context.BatchFee.Remove(batchFee);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BatchFeeExists(int id)
+        {
+            return _context.BatchFee.Any(e => e.FeeId == id);
+        }
+    }
+}
---
+++ TuitionDb/Controllers/BatchesController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TuitionDb.Areas.Identity.Data;
+using TuitionDbv1.Models;
+
+namespace TuitionDb.Controllers
+{
+    public class BatchesController : Controller
+    {
+        private readonly TuitionDbContext _context;
+
+        public BatchesController(TuitionDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Batches
+        public async Task<IActionResult> Index()
+        {
+            var tuitionDbContext = _context.Batches.Include(b => b.Staffs);
+            return View(await tuitionDbContext.ToListAsync());
+        }
+
+        // GET: Batches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batch = await _context.Batches
+                .Include(b => b.Staffs)
+                .FirstOrDefaultAsync(m => m.BatchId == id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            return View(batch);
+        }
+
+        // GET: Batches/Create
+        public IActionResult Create()
+        {
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "StaffId", "StaffDesc");
+            return View();
+        }
+
+        // POST: Batches/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BatchId,BatchDay,BatchTime,BatchNotes,StaffId")] Batch batch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(batch);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "StaffId", "StaffDesc", batch.StaffId);
+            return View(batch);
+        }
+
+        // GET: Batches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batch = await _context.Batches.FindAsync(id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "StaffId", "StaffDesc", batch.StaffId);
+            return View(batch);
+        }
+
+        // POST: Batches/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BatchId,BatchDay,BatchTime,BatchNotes,StaffId")] Batch batch)
+        {
+            if (id != batch.BatchId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(batch);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BatchExists(batch.BatchId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "StaffId", "StaffDesc", batch.StaffId);
+            return View(batch);
+        }
+
+        // GET: Batches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var batch = await _context.Batches
+                .Include(b => b.Staffs)
+                .FirstOrDefaultAsync(m => m.BatchId == id);
+            if (batch == null)
+            {
+                return NotFound();
+            }
+
+            return View(batch);
+        }
+
+        // POST: Batches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var batch = await _context.Batches.FindAsync(id);
+            if (batch != null)
+            {
+                _context.Batches.Remove(batch);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BatchExists(int id)
+        {
+            return _context.Batches.Any(e => e.BatchId == id);
+        }
+    }
+}
